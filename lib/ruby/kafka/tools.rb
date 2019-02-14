94ef1f9a340336d576f6c8bb30d37eda574d89aa
Dapeng Li <luislee818@gmail.com>
2019-02-14 13:50:54 +0100

    initial commit

---
+++ lib/ruby/kafka/tools.rb
@@ -0,0 +1,10 @@
+require "ruby/kafka/tools/version"
+
+module Ruby
+  module Kafka
+    module Tools
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/kafka/tools/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Kafka
+    module Tools
+      VERSION = "0.1.0"
+    end
+  end
+end
